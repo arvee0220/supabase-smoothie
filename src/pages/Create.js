566318d422b0cc91136c7713fa4730d96b2c77d5
arvee0220 <rowellverdejo@gmail.com>
2025-01-20 23:37:22 +0800

    Add post method

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,69 @@
-const Create = () => {
-  return (
-    <div className="page create">
-      <h2>Create</h2>
-    </div>
-  )
-}
+import { useState } from "react";
+import supabase from "../config/supabaseClient";
 
-export default Create
+const Create = () => {
+	const [title, setTitle] = useState("");
+	const [method, setMethod] = useState("");
+	const [rating, setRating] = useState("");
+	const [formError, setFormError] = useState(null);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (!title || !method || !rating) {
+			setFormError("Please fill in all the fields correctly");
+			return;
+		}
+
+		console.log("Submitting data:", { title, method, rating });
+
+		try {
+			const { data, error } = await supabase
+				.from("smoothies")
+				.insert([{ title, method, rating: parseInt(rating, 10) }])
+				.select("*");
+
+			console.log("Response:", { data, error });
+
+			if (error) {
+				console.error("Insert error:", error);
+				setFormError("An error occurred while saving the smoothie.");
+			} else {
+				console.log("Inserted data:", data);
+				setFormError(null);
+			}
+		} catch (err) {
+			console.error("Unexpected error:", err);
+		}
+	};
+
+	return (
+		<div className="page create">
+			<h2>Create</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="title">Title:</label>
+				<input
+					type="text"
+					id="title"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+
+				<label htmlFor="method">Method:</label>
+				<textarea id="method" value={method} onChange={(e) => setMethod(e.target.value)} />
+
+				<label htmlFor="rating">Rating:</label>
+				<input
+					type="number"
+					id="rating"
+					value={rating}
+					onChange={(e) => setRating(e.target.value)}
+				/>
+				<button type="submit">Create Smoothie Recipe</button>
+				{formError && <p className="error">{formError}</p>}
+			</form>
+		</div>
+	);
+};
+
+export default Create;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,49 @@
-const Home = () => {
-  return (
-    <div className="page home">
-      <h2>Home</h2>
-    </div>
-  )
-}
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+import SmoothieCard from "../components/SmoothieCard";
 
-export default Home
+const Home = () => {
+	const [fetchError, setFetchError] = useState(null);
+	const [smoothies, setSmoothies] = useState(null);
+
+	useEffect(() => {
+		const fetchSmoothies = async () => {
+			try {
+				const { data, error } = await supabase.from("smoothies").select();
+
+				if (error) {
+					setFetchError("Could not fetch smoothies");
+					console.log(error);
+					setSmoothies(null);
+				}
+
+				if (data) {
+					setSmoothies(data);
+					setFetchError(null);
+				}
+			} catch (e) {
+				console.table(e);
+			}
+		};
+
+		fetchSmoothies();
+	}, []);
+
+	return (
+		<div className="page home">
+			<h2>Home</h2>
+			{fetchError && <p>{fetchError}</p>}
+			{smoothies && (
+				<div className="smoothies">
+					<div className="smoothie-grid">
+						{smoothies.map((smoothie) => (
+							<SmoothieCard smoothie={smoothie} key={smoothie.id} />
+						))}
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Home;
